2e5e171597f439c96e602a26072b7bd97b62e9f3
Erik Roganský <roganskyerik@gmail.com>
2024-09-06 10:50:01 +0200

    Backend captcha integration

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use App\Mail\SendMail;
 use App\Mail\SendMessageToEndUser;
@@ -25,15 +26,43 @@ class MailController extends Controller {
             return redirect('/');
         }
 
+        $validator = Validator::make($input, [
+            'g-recaptcha-response' => 'required|recaptchav3:send_message,0.5'
+        ]);
+
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|email',
             'phone' => 'nullable|max:32',
             'subject' => 'required|string',
-            'message' => 'required|string',
-            'g-recaptcha-response' => 'required|recaptchav3:send_message,0.5'
+            'message' => 'required|string'
+        ]);
+        
+        $token = $request->input('g-recaptcha-response');
+
+ 
+        $response = Http::post('https://recaptchaenterprise.googleapis.com/v1/projects/my-website-recap-1725601492278/assessments?key=AIzaSyBbc_EoWhWB7dl9fWQAOJ5w_-gy8s9pSxw', [
+             'event' => [
+                'token' => $token,
+                'siteKey' => "6Ld2_zcqAAAAAPO4FWBhvwnSrWKSXYiMRjhKiGQQ",
+            ],
         ]);
 
+        $body = $response->json();
+        
+        if (isset($body['riskAnalysis']['score'])) {
+            $score = $body['riskAnalysis']['score'];
+            
+            if ($score < 0.5) {
+                $request->session()->flash('successMessage', "You failed reCAPTCHA.");
+                return redirect('/');
+            }
+        } else {
+            \Log::error('ReCAPTCHA response is missing the riskAnalysis score');
+            $request->session()->flash('successMessage', "reCAPTCHA validation failed");
+            return redirect('/');
+        }  
+        
         $userComment = new FormUser();
         $userComment->name = $request->input('name');
         $userComment->email = $request->input('email');
